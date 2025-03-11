e49e6cbe0894352fad65092b12fad77b6a17ceb4
moritzgauss <moritzgg99@gmail.com>
2025-03-11 23:12:02 +0100

    lol

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     // Cover page click to navigate to first spread
     const magazineCover = document.querySelector('.magazine-cover');
@@ -14,6 +13,19 @@ document.addEventListener('DOMContentLoaded', function() {
         arrow.addEventListener('click', function(e) {
             // Could add sound effect here if desired
             // Currently just for future enhancement
+        });
+    });
+
+    document.querySelectorAll('.nav-arrow').forEach(arrow => {
+        arrow.addEventListener('click', function() {
+            const currentPage = document.querySelector('.page.current');
+            const nextPage = this.classList.contains('next-arrow') ? currentPage.nextElementSibling : currentPage.previousElementSibling;
+
+            if (nextPage) {
+                currentPage.classList.remove('current');
+                currentPage.classList.add(this.classList.contains('next-arrow') ? 'flip' : 'flip-back');
+                nextPage.classList.add('current');
+            }
         });
     });
 
